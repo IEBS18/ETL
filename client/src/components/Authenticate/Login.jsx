db92501394738729a03a3608f3fbbe3cf0c3d871
IEBS15 <ayush.kumar@iebrain.com>
2024-09-14 22:18:46 +0530

    deployment 1

--- client/src/components/Authenticate/Login.jsx
+++ client/src/components/Authenticate/Login.jsx
@@ -18,7 +18,7 @@ function Login({ onSwitchToSignup }) {
 
         try {
             // Send a POST request to the backend
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch('http://54.234.139.165:5000/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
